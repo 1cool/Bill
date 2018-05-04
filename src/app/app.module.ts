a2e2b3c975ebb7e038a6b7b4bb162a69d55a3cda
qiqj <1824964246@qq.com>
2018-05-04 15:21:30 +0800

    添加SUI Mobile的css支持

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
